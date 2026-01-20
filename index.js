2ffcda40f45cf3b838404a75eba8673155a13813
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-20 13:04:08 -0600

    Fix square function and update its usage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function square (1, 1){
-     return 1 - 1;
-}
-document.getElementId("result").innerHTML = square (1, 1);
+let square = function (1. 5){
+     return 1 + 4;
+}  
+document.getElementId("result").innerHTML = square (1, 5);
+
