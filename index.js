b832c15bee7acf9c6794abdd2aa4a15ab4a752b1
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-22 14:57:00 -0600

    changed function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let square = function (1, 1){
-     return 1 + 1;
+function square (a) {
+     return a*a;
 }  
-document.getElementById("result").innerHTML = square(1, 1);
+document.getElementById("result").innerHTML = square(5);
+
 
 
 
